1ecdcb4849d3ee618435bb1281716ac81d7759f5
Richard Todosichuk <rtodosic@hotmail.com>
2025-01-26 17:06:49 -0500

    fix: minor refactors

--- CosmosCli/Validations/ValidateAutoscaleThroughputAttribute.cs
+++ CosmosCli/Validations/ValidateAutoscaleThroughputAttribute.cs
@@ -10,10 +10,12 @@ class ValidateAutoscaleThroughputAttribute : ValidationAttribute
         {
             return new ValidationResult($"Autoscale throughput values must be between 1000 and 1000000 inclusive in increments of 1000. {value} is less than required minimum throughput of 1000.");
         }
+
         if (value is int and > 1000000)
         {
             return new ValidationResult($"Autoscale throughput values must be between 1000 and 1000000 inclusive in increments of 1000. {value} is greater than required maximum throughput of 1000000.");
         }
+
         if (value is int && ((int)value % 1000) != 0)
         {
             return new ValidationResult($"Autoscale throughput values must be between 1000 and 1000000 inclusive in increments of 1000. {value} is not an increment of 1000.");
--- CosmosCli/Validations/ValidateManualThoughputAttribute.cs
+++ CosmosCli/Validations/ValidateManualThoughputAttribute.cs
@@ -10,10 +10,12 @@ class ValidateManualThroughputAttribute : ValidationAttribute
         {
             return new ValidationResult($"Manual throughput values must be between 400 and 1000000 inclusive in increments of 100. {value} is less than required minimum throughput of 400.");
         }
+
         if (value is int and > 1000000)
         {
             return new ValidationResult($"Manual throughput values must be between 400 and 1000000 inclusive in increments of 100. {value} is greater than required maximum throughput of 1000000.");
         }
+
         if (value is int && ((int)value % 100) != 0)
         {
             return new ValidationResult($"Manual throughput values must be between 400 and 1000000 inclusive in increments of 100. {value} is not an increment of 100.");
